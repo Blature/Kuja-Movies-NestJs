65b8578239c377376afbbcaf357427a72fe44794
Ho3yn.Golchin <96630646+Blature@users.noreply.github.com>
2022-07-23 02:12:24 +0430

    Update Auth Service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialDto } from './dto/auth-credentials.dto';
+import { UsersRepository } from './users.repository';
+import * as bcrypt from 'bcrypt';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(UsersRepository) private usersRepository: UsersRepository,
+    private jwtService: JwtService,
+  ) {}
+
+  async signUp(authCredentialDto: AuthCredentialDto): Promise<void> {
+    return this.usersRepository.createUser(authCredentialDto);
+  }
+
+  async signIn(
+    authCredentialDto: AuthCredentialDto,
+  ): Promise<{ accessToken: string }> {
+    const { username, password } = authCredentialDto;
+    const user = await this.usersRepository.findOne({ username });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const payload: JwtPayload = { username };
+      const accessToken: string = await this.jwtService.sign(payload);
+      return { accessToken };
+    } else {
+      throw new UnauthorizedException(
+        'Your Username or Password is Incorrect !',
+      );
+    }
+  }
+}

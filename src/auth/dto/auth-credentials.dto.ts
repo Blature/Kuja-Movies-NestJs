abac06ec04cd5bd999b91169e8c367a90f8aef08
Ho3yn.Golchin <96630646+Blature@users.noreply.github.com>
2022-07-22 23:22:58 +0430

    Create Credentials for Auth

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,24 @@
+import {
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class AuthCredentialDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(4)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password is too weak,try again with stronger one !',
+  })
+  password: string;
+}
